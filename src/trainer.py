6ea5c36a0ab3179fcca39dd102fad09481647c66
KNakane <koki.bsb@gmail.com>
2021-03-08 21:47:11 +0900

    [update] implement multi task

--- src/trainer.py
+++ src/trainer.py
@@ -12,7 +12,7 @@ class Trainer():
     def __init__(self,
                  FLAGS,
                  message,
-                 data,
+                 datasets,
                  model,
                  name):
         self.checkpoints_to_keep = FLAGS.checkpoints_to_keep
@@ -22,7 +22,8 @@ class Trainer():
         self.batch_size = FLAGS.batch_size
         self.name = name
         self.message = message
-        self.data = data
+        self.data = datasets[0]
+        self.datasets = datasets
         self.global_step = tf.train.get_or_create_global_step()
         self.model = model
         self.method = FLAGS.method
@@ -47,29 +48,25 @@ class Trainer():
         # train
         self.train_logits = self.model.inference(train_data)
         self.train_loss = self.model.loss(self.train_logits, train_ans)
-        #self.predict = self.model.predict(train_data)
         opt_op = self.model.optimize(self.train_loss, self.global_step)
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         self.train_op = tf.group([opt_op] + update_ops)
 
         self.train_loss_ = self.model.loss(self.train_logits, train_ans, mode=self.method)
-        opt_op_ewc = self.model.optimize(self.train_loss_, self.global_step)
-        self.train_op_ewc = tf.group([opt_op_ewc] + update_ops)
+        reopt_op = self.model.optimize(self.train_loss_, self.global_step)
+        self.retrain_op = tf.group([reopt_op] + update_ops)
 
-        self.train_accuracyA = self.model.evaluate(self.train_logits, train_ans, prefix='A')
-        self.train_accuracyB = self.model.evaluate(self.train_logits, train_ans, prefix='B')
+        self.train_accuracy = self.model.evaluate(self.train_logits, train_ans)
 
         # validation
         self.valid_logits = self.model.test_inference(valid_data, reuse=True)
         self.valid_loss = self.model.loss(self.valid_logits, valid_ans)
-        self.valid_accuracyA = self.model.evaluate(self.valid_logits, valid_ans, prefix='A')
-        self.valid_accuracyB = self.model.evaluate(self.valid_logits, valid_ans, prefix='B')
+        self.valid_accuracy = self.model.evaluate(self.valid_logits, valid_ans)
 
         # test
         self.test_logits = self.model.test_inference(test_data, reuse=True)
         self.test_loss = self.model.loss(self.test_logits, test_ans)
-        self.test_accuracyA = self.model.evaluate(self.test_logits, test_ans, prefix='A')
-        self.test_accuracyB = self.model.evaluate(self.test_logits, test_ans, prefix='B')
+        self.test_accuracy = self.model.evaluate(self.test_logits, test_ans)
 
         return
 
@@ -95,15 +92,12 @@ class Trainer():
         """
         # tensorboard
         tf.summary.scalar('train/loss', self.train_loss)
-        tf.summary.scalar('train/accuracyA', self.train_accuracyA)
-        tf.summary.scalar('train/accuracyB', self.train_accuracyB)
+        tf.summary.scalar('train/accuracy', self.train_accuracy)
         tf.summary.scalar('train/Learning_rate', self.model.optimizer.lr)
         tf.summary.scalar('valid/loss', self.valid_loss)
-        tf.summary.scalar('valid/accuracyA', self.valid_accuracyA)
-        tf.summary.scalar('valid/accuracyB', self.valid_accuracyB)
+        tf.summary.scalar('valid/accuracy', self.valid_accuracy)
         tf.summary.scalar('test/loss', self.test_loss)
-        tf.summary.scalar('test/accuracyA', self.test_accuracyA)
-        tf.summary.scalar('test/accuracyB', self.test_accuracyB)
+        tf.summary.scalar('test/accuracy', self.test_accuracy)
         if len(train_inputs.shape) == 4:
             tf.summary.image('train/image', train_inputs)
             tf.summary.image('valid/image', valid_inputs)
@@ -117,9 +111,6 @@ class Trainer():
         (train_inputs, train_labels), (valid_inputs, valid_labels), (test_inputs, test_labels) = self.load()
         self.build_logits(train_inputs, train_labels, valid_inputs, valid_labels, test_inputs, test_labels)
         self.build_fissher_info(valid_inputs, valid_labels)
-        
-        # retrain
-        #self.build_relogits(train_inputs, train_labels, valid_inputs, valid_labels, test_inputs, test_labels)
 
         merged = self.summary(train_inputs, valid_inputs, test_inputs)
 
@@ -148,109 +139,59 @@ class Trainer():
                 print('Not restore init model')
 
 
-            # Training TaskA
+            # Training each task
             start = time.time()
-            for step in range(1, self.max_steps+1):
-                iteration_time = time.time()
-                _, x_train, y_train = self.data.next_batch(batch_size=self.batch_size)
-                _, x_valid, y_valid = self.data.next_batch(batch_size=self.batch_size*3, valid=True)
-                _, x_test, y_test = self.data.next_batch(batch_size=self.batch_size*3, test=True)
-                _, summary = session.run([self.train_op, merged],
-                                         feed_dict={
-                                             train_inputs: x_train,
-                                             train_labels: y_train,
-                                             valid_inputs: x_valid,
-                                             valid_labels: y_valid,
-                                             test_inputs: x_test,
-                                             test_labels: y_test
-                                         })
-                iteration_end = time.time() - iteration_time
-                summary_time = session.run(board4time, feed_dict={step_time:iteration_end})
-                writer.add_summary(summary, step)
-                writer.add_summary(summary_time, step)
+            for n_task, data in enumerate(self.datasets):
+                train_op = self.retrain_op if n_task else self.train_op
+                for step in range(1, self.max_steps+1):
+                    iteration_time = time.time()
+                    _, x_train, y_train = data.next_batch(batch_size=self.batch_size)
+                    _, train_loss, train_acc = session.run([train_op, self.train_loss, self.train_accuracy],
+                                                           feed_dict={
+                                                             train_inputs: x_train,
+                                                             train_labels: y_train
+                                                           })
+                    iteration_end = time.time() - iteration_time
+                    summary_time = session.run(board4time, feed_dict={step_time:iteration_end})
+                    #writer.add_summary(summary, step)
+                    writer.add_summary(summary_time, step)
 
-                if step == 1 or step % 100 == 0:
-                    train_loss, train_acc, valid_loss, valid_acc, test_loss, test_acc = session.run(
-                        [self.train_loss, self.train_accuracyA, self.valid_loss, self.valid_accuracyA, self.test_loss, self.test_accuracyA],
-                        feed_dict={
-                            train_inputs: x_train,
-                            train_labels: y_train,
-                            valid_inputs: x_valid,
-                            valid_labels: y_valid,
-                            test_inputs: x_test,
-                            test_labels: y_test
-                        }
-                    )
+                    if step == 1 or step % 100 == 0:
+                        metrics = OrderedDict({
+                            "global_step": step,
+                            "train loss": '{:.3f}'.format(train_loss),
+                            "train accuracy":'{:.3f}'.format(train_acc)
+                        })
 
-                    metrics = OrderedDict({
-                        "global_step": step,
-                        "train loss": '{:.3f}'.format(train_loss),
-                        "train accuracy":'{:.3f}'.format(train_acc),
-                        "valid loss": '{:.3f}'.format(valid_loss),
-                        "valid accuracy": '{:.3f}'.format(valid_acc),
-                        "test loss": '{:.3f}'.format(test_loss),
-                        "test accuracy": '{:.3f}'.format(test_acc),
-                        "time_per_epoch": '{:.3f}'.format(iteration_end)})
+                        for i, test_data in enumerate(self.datasets):
+                            _, x_test, y_test = test_data.next_batch(batch_size=10000, test=True)
+                            test_loss, test_acc = session.run(
+                                [self.test_loss, self.test_accuracy],
+                                feed_dict={
+                                    test_inputs: x_test,
+                                    test_labels: y_test
+                                }
+                            )
+                            metrics["test_loss{}".format(i+1)] = '{:.3f}'.format(test_loss)
+                            metrics["test_acc{}".format(i+1)] = '{:.3f}'.format(test_acc)
 
-                    self.util.write_log(message=metrics, cout=True)
-                    saver.save(session, self.util.model_path + '/model', global_step=step)
+                    
+                        metrics["time_per_epoch"] = '{:.3f}'.format(iteration_end)
 
-            # save model parameter
-            valid_num = 1000
-            _, x_valid, y_valid = self.data.next_batch(batch_size=valid_num, valid=True)
-            gradients = session.run(self.grads, feed_dict={valid_inputs: x_valid, valid_labels: y_valid})
-            self.model.fissher_info(gradients, valid_num)
-            self.model.set_old_val()
+                        self.util.write_log(message=metrics, cout=True)
+                        saver.save(session, self.util.model_path + '/model', global_step=step)
 
-            # Training TaskB
-            Accuracy_A, Accuracy_B = [], []
-            for step in range(1, self.max_steps+1):
-                iteration_time = time.time()
-                _, x_train, y_train = self.data.next_batch(batch_size=self.batch_size, perm=True)
-                _, x_valid, y_valid = self.data.next_batch(batch_size=self.batch_size*20, valid=True, perm=True)
-                _, x_test, y_test = self.data.next_batch(batch_size=self.batch_size*20, test=True)
-                _, summary = session.run([self.train_op_ewc, merged],
-                                         feed_dict={
-                                             train_inputs: x_train,
-                                             train_labels: y_train,
-                                             valid_inputs: x_valid,
-                                             valid_labels: y_valid,
-                                             test_inputs: x_test,
-                                             test_labels: y_test
-                                         })
-                iteration_end = time.time() - iteration_time
-                summary_time = session.run(board4time, feed_dict={step_time:iteration_end})
-                writer.add_summary(summary, step)
-                writer.add_summary(summary_time, step)
+                # save model parameter
+                valid_num = 1000
+                _, x_valid, y_valid = data.next_batch(batch_size=valid_num, valid=True)
+                gradients = session.run(self.grads, feed_dict={valid_inputs: x_valid, valid_labels: y_valid})
+                self.model.fissher_info(gradients, valid_num)
+                self.model.set_old_val()
 
-                if step == 1 or step % 100 == 0:
-                    train_loss, train_acc, valid_acc, test_acc = session.run(
-                        [self.train_loss, self.train_accuracyB, self.test_accuracyA, self.valid_accuracyB],
-                        feed_dict={
-                            train_inputs: x_train,
-                            train_labels: y_train,
-                            valid_inputs: x_valid,
-                            valid_labels: y_valid,
-                            test_inputs: x_test,
-                            test_labels: y_test
-                        }
-                    )
-                    Accuracy_A.append(valid_acc)
-                    Accuracy_B.append(test_acc)
-
-                    metrics = OrderedDict({
-                        "global_step": step,
-                        "train loss": '{:.3f}'.format(train_loss),
-                        "train accuracy":'{:.3f}'.format(train_acc),
-                        "accuracyA": '{:.3f}'.format(valid_acc),
-                        "accuracyB": '{:.3f}'.format(test_acc),
-                        "time_per_epoch": '{:.3f}'.format(iteration_end)})
-
-                    self.util.write_log(message=metrics, cout=True)
 
             elapsed_time = time.time() - start
             print(elapsed_time)
-            self.progress_graph(Accuracy_A, Accuracy_B)
+            #self.progress_graph(Accuracy_A, Accuracy_B)
             writer.close()
 
         return 
